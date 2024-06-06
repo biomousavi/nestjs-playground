0f999ed42718ed24604834836264f4ff0b891e31
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-06-06 18:56:23 +0330

    feat: add auth service

---
+++ reservation/apps/auth/src/users/users.module.ts
@@ -0,0 +1,18 @@
+import { DatabaseModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+  ],
+  providers: [UsersService, UsersRepository],
+  controllers: [UsersController],
+})
+export class UsersModule {}
