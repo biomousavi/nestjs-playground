eebc935d6381d3ce7b3475e8fd4035fc73839b7d
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-06-06 21:57:38 +0330

    refactor: decouple configs

--- reservation/apps/auth/src/auth.module.ts
+++ reservation/apps/auth/src/auth.module.ts
@@ -5,14 +5,23 @@ import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     UsersModule,
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
     JwtModule.registerAsync({
       inject: [ConfigService],
-      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
--- reservation/apps/reservation/src/reservation.module.ts
+++ reservation/apps/reservation/src/reservation.module.ts
@@ -3,15 +3,25 @@ import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationRepository } from './reservation.repository';
+import * as Joi from 'joi';
+
 import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     DatabaseModule,
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
