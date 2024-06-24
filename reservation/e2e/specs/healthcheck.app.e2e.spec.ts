c3c68f5607c5ab85d9edb71944e3a9bc2847debb
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-06-24 12:13:07 +0330

    test: auth service

--- reservation/e2e/specs/healthcheck.app.e2e.spec.ts
+++ reservation/e2e/specs/healthcheck.app.e2e.spec.ts
@@ -4,7 +4,16 @@ describe('Health', () => {
       const res = await fetch('http://reservations:3002/health');
       expect(res.ok).toBeTruthy();
     } catch (error) {
-      console.log(error, 'eeeeeeeee');
+      console.log(error);
+    }
+  });
+
+  test('Auth', async () => {
+    try {
+      const res = await fetch('http://auth:3001/health');
+      expect(res.ok).toBeTruthy();
+    } catch (error) {
+      console.log(error);
     }
   });
 });
